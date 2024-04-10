cc639e0516983912a7fd32ced15afd43175c03e7
장 재 훈 <peng2412@naver.com>
2024-04-10 21:54:51 +0900

    [Feat] -> MoviePage, 페이지네이션 기능 추가

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { useSearchMovieQuery } from '../../hooks/useSearchMovie'
 import { useSearchParams } from 'react-router-dom'
 import { Alert } from 'bootstrap'
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import MovieCard from '../../common/MovieCard/MovieCard'
+import ReactPaginate from 'react-paginate';
 // 경로 2가지
 // navBar에서 클릭해서 온경우 => popularMovie 보여주기
 // keyword를 입력해서 온경우 => keyword와 관련된 영화들을 보여줌
@@ -16,9 +17,15 @@ import MovieCard from '../../common/MovieCard/MovieCard'
 // page 값이 바뀔때 마다 useSearchMovie에 page까지 넣어서 fetch
 const MoviePage = () => {
     const [query, setQuery] = useSearchParams()
+    const [page, setPage] = useState(1)
     const keyword = query.get("q")
 
-    const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword })
+    const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword, page })
+
+    const handlePageClick = (page) => {
+        console.log("ppp", page)
+        setPage(page.selected + 1)
+    }
 
     if (isLoading) {
         return <h1>Loading...</h1>
@@ -42,6 +49,27 @@ const MoviePage = () => {
                             </Col>)}
                     </Row>
                 </Col>
+                <ReactPaginate
+                    nextLabel="next >"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    marginPagesDisplayed={2}
+                    pageCount={data?.total_pages} //전체 페이지
+                    previousLabel="< previous"
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakLabel="..."
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                    renderOnZeroPageCount={null}
+                    forcePage={page - 1}
+                />
             </Row>
         </Container>
     )
