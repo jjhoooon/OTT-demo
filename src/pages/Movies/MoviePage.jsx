2a94d863d91af8341876f51d8905467d2574feff
장 재 훈 <peng2412@naver.com>
2024-04-13 18:31:41 +0900

    [Feat] -> MoviePage, 장르 필터링

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -7,6 +7,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import MovieCard from '../../common/MovieCard/MovieCard'
 import ReactPaginate from 'react-paginate';
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre'
+import GenreButton from './components/GenreButton/GenreButton'
 // 경로 2가지
 // navBar에서 클릭해서 온경우 => popularMovie 보여주기
 // keyword를 입력해서 온경우 => keyword와 관련된 영화들을 보여줌
@@ -19,9 +21,14 @@ const MoviePage = () => {
     const [sortOrder, setSortOrder] = useState(false)
     const [query, setQuery] = useSearchParams()
     const [page, setPage] = useState(1)
+    const [genreFilter, setGenreFilter] = useState("")
+    const [filteredMovies, setFilteredMovies] = useState([])
     const keyword = query.get("q")
 
     const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword, page })
+    const { data: genreData } = useMovieGenreQuery()
+
+    console.log("data", data?.results.genre_ids)
 
     const handlePageClick = (page) => {
         console.log("ppp", page)
@@ -36,8 +43,15 @@ const MoviePage = () => {
     }
 
     useEffect(() => {
+        if (genreFilter) {
+            const filtered = data?.results.filter(movie => movie.genre_ids.includes(parseInt(genreFilter)));
+            setFilteredMovies(filtered);
+        } else {
+            setFilteredMovies(data?.results);
+        }
+    }, [data, genreFilter]);
 
-    }, [sortOrder])
+
 
     if (sortOrder) {
         data?.results.sort((a, b) => a.popularity - b.popularity)
@@ -63,14 +77,24 @@ const MoviePage = () => {
             <Row>
                 <Col lg={4} xs={12}>
                     <button onClick={handleSortMovie}>인기도 오름차순 정렬</button>
+                    <div>
+                        {genreData?.map((genre, index) =>
+                            <GenreButton key={index} genre={genre} genreFilter={genreFilter} setGenreFilter={setGenreFilter} />
+                        )}
+                    </div>
                 </Col>
                 <Col lg={8} xs={12}>
                     {/* 카드의 정렬을 위한 Row */}
                     <Row>
-                        {data?.results.map((movie, index) => movie.backdrop_path &&
+                        {genreFilter && filteredMovies.map((movie, index) => movie.backdrop_path &&
                             <Col key={index} lg={4} xs={12}>
                                 <MovieCard movie={movie} />
                             </Col>)}
+                        {!genreFilter && data?.results.map((movie, index) => movie.backdrop_path &&
+                            <Col key={index} lg={4} xs={12}>
+                                <MovieCard movie={movie} />
+                            </Col>)}
+
                     </Row>
                 </Col>
                 <ReactPaginate
